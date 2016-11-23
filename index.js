063e98d809b0d470bea6eba8d70def08d08f32df
Mathieu Dutour <mathieu@dutour.me>
2016-11-23 19:26:21 +0000

    :tada: first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  globals: {
+    NSImage: false,
+    NSImage: false,
+    NSUserDefaults: false,
+    NSTask: false,
+    NSPipe: false,
+    NSString: false,
+    NSUTF8StringEncoding: false,
+    NSAlert: false,
+    NSURL: false,
+    NSWorkspace: false,
+    NSView: false,
+    NSMakeRect: false,
+    NSTextField: false,
+    NSButton: false,
+    NSComboBox: false,
+    NSBundle: false,
+    NSScrollView: false,
+    NSLineBorder: false,
+    NSTextView: false,
+    NSMakeSize: false,
+    NSApp: false,
+    NSArray: false,
+    NSException: false,
+  },
+
+  rules: {
+    eqeqeq: [0]
+  }
+};
