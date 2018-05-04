c4b307c662b907d636168940de7eaabd2312c504
mathieudutour <mathieu@dutour.me>
2018-05-04 09:56:42 +0200

    add some more injected constants

--- build.js
+++ build.js
@@ -3,14 +3,17 @@ const fs = require('fs')
 
 const now = Date.now()
 
+const SKETCH_PATH = '/Applications/Sketch.app/Contents/MacOS/Sketch'
+const MACOS_SDK = '/System/Library/Frameworks'
+
 exec('rm -rf .headers').catch(() => {})
 .then(() => {
   console.log('[1/4] Generating Sketch headers')
-  return exec('./class-dump -H /Applications/Sketch.app/Contents/MacOS/Sketch -o ./.headers -C "^[a-zA-Z]*$"')
+  return exec(`./class-dump -H "${SKETCH_PATH}" -o ./.headers -C "^[a-zA-Z]*$"`)
 })
 .then(() => {
   console.log('[2/4] Generating macOS headers')
-  return exec('./class-dump -H --sdk-mac 10.12 -o ./.headers -C "^[a-zA-Z]*$"')
+  return exec(`./class-dump -H "${MACOS_SDK}/AppKit.framework" -o ./.headers -C "^[a-zA-Z]*$" && ./class-dump -H "${MACOS_SDK}/Foundation.framework" -o ./.headers -C "^[a-zA-Z]*$"`)
 })
 .then(() => {
   console.log('[3/4] Reading generating files')
